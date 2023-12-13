ae0b8ce6d26460462e472711d6dd40537454c107
So Nita <sonita.borin2@gmail.com>
2023-12-13 10:55:33 +0700

    .

--- POS_WebAPI/obj/Debug/net8.0/POS_WebAPI.AssemblyInfo.cs
+++ POS_WebAPI/obj/Debug/net8.0/POS_WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POS_WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+854649059edaa16f0ea43a11c4c8973a85a5f180")]
 [assembly: System.Reflection.AssemblyProductAttribute("POS_WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POS_WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
