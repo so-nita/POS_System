669e4cde957121f1c7f3decfeb479f561b8e308f
So Nita <sonita.borin2@gmail.com>
2023-12-13 08:12:04 +0700

    Committing changes before switching branches

--- POS_WebAPI/obj/Debug/net8.0/POS_WebAPI.AssemblyInfo.cs
+++ POS_WebAPI/obj/Debug/net8.0/POS_WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POS_WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0c389886f5386e7dd239e757d346da7cc159147")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("POS_WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POS_WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
