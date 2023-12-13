305166583695e237b26461412734d59dcfa915e7
So Nita <sonita.borin2@gmail.com>
2023-12-13 11:03:24 +0700

    UpdateDate : 13-12-2023

---
+++ POS_Desktop/View/MainContainerView.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace POS_Desktop.View
+{
+    partial class MainContainerView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            nightControlBox1 = new ReaLTaiizor.Controls.NightControlBox();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            bunifuFlatButton1 = new Bunifu.Framework.UI.BunifuFlatButton();
+            bunifuFlatButton2 = new Bunifu.Framework.UI.BunifuFlatButton();
+            flowLayoutPanel2 = new FlowLayoutPanel();
+            bunifuFlatButton3 = new Bunifu.Framework.UI.BunifuFlatButton();
+            bunifuFlatButton4 = new Bunifu.Framework.UI.BunifuFlatButton();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            flowLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(nightControlBox1);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1038, 35);
+            panel1.TabIndex = 0;
+            // 
+            // nightControlBox1
+            // 
+            nightControlBox1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            nightControlBox1.BackColor = Color.Transparent;
+            nightControlBox1.CloseHoverColor = Color.FromArgb(199, 80, 80);
+            nightControlBox1.CloseHoverForeColor = Color.White;
+            nightControlBox1.DefaultLocation = true;
+            nightControlBox1.DisableMaximizeColor = Color.FromArgb(105, 105, 105);
+            nightControlBox1.DisableMinimizeColor = Color.FromArgb(105, 105, 105);
+            nightControlBox1.EnableCloseColor = Color.FromArgb(160, 160, 160);
+            nightControlBox1.EnableMaximizeButton = true;
+            nightControlBox1.EnableMaximizeColor = Color.FromArgb(160, 160, 160);
+            nightControlBox1.EnableMinimizeButton = true;
+            nightControlBox1.EnableMinimizeColor = Color.FromArgb(160, 160, 160);
+            nightControlBox1.Location = new Point(899, 0);
+            nightControlBox1.MaximizeHoverColor = Color.FromArgb(15, 255, 255, 255);
+            nightControlBox1.MaximizeHoverForeColor = Color.White;
+            nightControlBox1.MinimizeHoverColor = Color.FromArgb(15, 255, 255, 255);
+            nightControlBox1.MinimizeHoverForeColor = Color.White;
+            nightControlBox1.Name = "nightControlBox1";
+            nightControlBox1.Size = new Size(139, 31);
+            nightControlBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(58, 7);
+            label1.Name = "label1";
+            label1.Size = new Size(157, 19);
+            label1.TabIndex = 2;
+            label1.Text = "ICON | MODERN MENU";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.Hamber_icon;
+            pictureBox1.Location = new Point(10, 4);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(41, 28);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.BackColor = Color.White;
+            flowLayoutPanel1.Controls.Add(bunifuFlatButton1);
+            flowLayoutPanel1.Controls.Add(bunifuFlatButton2);
+            flowLayoutPanel1.Controls.Add(flowLayoutPanel2);
+            flowLayoutPanel1.Controls.Add(bunifuFlatButton3);
+            flowLayoutPanel1.Controls.Add(bunifuFlatButton4);
+            flowLayoutPanel1.Dock = DockStyle.Left;
+            flowLayoutPanel1.Location = new Point(0, 35);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(210, 526);
+            flowLayoutPanel1.TabIndex = 1;
+            // 
+            // bunifuFlatButton1
+            // 
+            bunifuFlatButton1.Active = false;
+            bunifuFlatButton1.Activecolor = Color.FromArgb(46, 139, 87);
+            bunifuFlatButton1.BackColor = Color.FromArgb(163, 192, 245);
+            bunifuFlatButton1.BackgroundImageLayout = ImageLayout.Stretch;
+            bunifuFlatButton1.BorderRadius = 0;
+            bunifuFlatButton1.ButtonText = "       Home";
+            bunifuFlatButton1.DisabledColor = Color.Gray;
+            bunifuFlatButton1.Dock = DockStyle.Top;
+            bunifuFlatButton1.Iconcolor = Color.FromArgb(163, 192, 245);
+            bunifuFlatButton1.Iconimage = Properties.Resources.home_con;
+            bunifuFlatButton1.Iconimage_right = null;
+            bunifuFlatButton1.Iconimage_right_Selected = null;
+            bunifuFlatButton1.Iconimage_Selected = null;
+            bunifuFlatButton1.IconMarginLeft = 0;
+            bunifuFlatButton1.IconMarginRight = 0;
+            bunifuFlatButton1.IconRightVisible = true;
+            bunifuFlatButton1.IconRightZoom = 0D;
+            bunifuFlatButton1.IconVisible = true;
+            bunifuFlatButton1.IconZoom = 50D;
+            bunifuFlatButton1.IsTab = false;
+            bunifuFlatButton1.Location = new Point(0, 0);
+            bunifuFlatButton1.Margin = new Padding(0);
+            bunifuFlatButton1.Name = "bunifuFlatButton1";
+            bunifuFlatButton1.Normalcolor = Color.FromArgb(163, 192, 245);
+            bunifuFlatButton1.OnHovercolor = Color.FromArgb(117, 160, 240);
+            bunifuFlatButton1.OnHoverTextColor = Color.White;
+            bunifuFlatButton1.Padding = new Padding(20, 0, 0, 0);
+            bunifuFlatButton1.selected = false;
+            bunifuFlatButton1.Size = new Size(210, 45);
+            bunifuFlatButton1.TabIndex = 2;
+            bunifuFlatButton1.Text = "       Home";
+            bunifuFlatButton1.TextAlign = ContentAlignment.MiddleLeft;
+            bunifuFlatButton1.Textcolor = Color.White;
+            bunifuFlatButton1.TextFont = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            // 
+            // bunifuFlatButton2
+            // 
+            bunifuFlatButton2.Active = false;
+            bunifuFlatButton2.Activecolor = Color.FromArgb(46, 139, 87);
+            bunifuFlatButton2.BackColor = Color.FromArgb(163, 192, 245);
+            bunifuFlatButton2.BackgroundImageLayout = ImageLayout.Stretch;
+            bunifuFlatButton2.BorderRadius = 0;
+            bunifuFlatButton2.ButtonText = "       Home";
+            bunifuFlatButton2.DisabledColor = Color.Gray;
+            bunifuFlatButton2.Dock = DockStyle.Top;
+            bunifuFlatButton2.Iconcolor = Color.FromArgb(163, 192, 245);
+            bunifuFlatButton2.Iconimage = Properties.Resources.product_icon;
+            bunifuFlatButton2.Iconimage_right = null;
+            bunifuFlatButton2.Iconimage_right_Selected = null;
+            bunifuFlatButton2.Iconimage_Selected = null;
+            bunifuFlatButton2.IconMarginLeft = 0;
+            bunifuFlatButton2.IconMarginRight = 0;
+            bunifuFlatButton2.IconRightVisible = true;
+            bunifuFlatButton2.IconRightZoom = 0D;
+            bunifuFlatButton2.IconVisible = true;
+            bunifuFlatButton2.IconZoom = 50D;
+            bunifuFlatButton2.IsTab = false;
+            bunifuFlatButton2.Location = new Point(0, 45);
+            bunifuFlatButton2.Margin = new Padding(0);
+            bunifuFlatButton2.Name = "bunifuFlatButton2";
+            bunifuFlatButton2.Normalcolor = Color.FromArgb(163, 192, 245);
+            bunifuFlatButton2.OnHovercolor = Color.FromArgb(117, 160, 240);
+            bunifuFlatButton2.OnHoverTextColor = Color.White;
+            bunifuFlatButton2.Padding = new Padding(20, 0, 0, 0);
+            bunifuFlatButton2.selected = false;
+            bunifuFlatButton2.Size = new Size(210, 45);
+            bunifuFlatButton2.TabIndex = 3;
+            bunifuFlatButton2.Text = "       Home";
+            bunifuFlatButton2.TextAlign = ContentAlignment.MiddleLeft;
+            bunifuFlatButton2.Textcolor = Color.White;
+            bunifuFlatButton2.TextFont = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            // 
+            // flowLayoutPanel2
+            // 
+            flowLayoutPanel2.Location = new Point(0, 90);
+            flowLayoutPanel2.Margin = new Padding(0);
+            flowLayoutPanel2.Name = "flowLayoutPanel2";
+            flowLayoutPanel2.Size = new Size(210, 150);
+            flowLayoutPanel2.TabIndex = 4;
+            // 
+            // bunifuFlatButton3
+            // 
+            bunifuFlatButton3.Active = false;
+            bunifuFlatButton3.Activecolor = Color.FromArgb(46, 139, 87);
+            bunifuFlatButton3.BackColor = Color.FromArgb(163, 192, 245);
+            bunifuFlatButton3.BackgroundImageLayout = ImageLayout.Stretch;
+            bunifuFlatButton3.BorderRadius = 0;
+            bunifuFlatButton3.ButtonText = "       Home";
+            bunifuFlatButton3.DisabledColor = Color.Gray;
+            bunifuFlatButton3.Dock = DockStyle.Top;
+            bunifuFlatButton3.Iconcolor = Color.FromArgb(163, 192, 245);
+            bunifuFlatButton3.Iconimage = Properties.Resources.home_con;
+            bunifuFlatButton3.Iconimage_right = null;
+            bunifuFlatButton3.Iconimage_right_Selected = null;
+            bunifuFlatButton3.Iconimage_Selected = null;
+            bunifuFlatButton3.IconMarginLeft = 0;
+            bunifuFlatButton3.IconMarginRight = 0;
+            bunifuFlatButton3.IconRightVisible = true;
+            bunifuFlatButton3.IconRightZoom = 0D;
+            bunifuFlatButton3.IconVisible = true;
+            bunifuFlatButton3.IconZoom = 60D;
+            bunifuFlatButton3.IsTab = false;
+            bunifuFlatButton3.Location = new Point(0, 240);
+            bunifuFlatButton3.Margin = new Padding(0);
+            bunifuFlatButton3.Name = "bunifuFlatButton3";
+            bunifuFlatButton3.Normalcolor = Color.FromArgb(163, 192, 245);
+            bunifuFlatButton3.OnHovercolor = Color.FromArgb(117, 160, 240);
+            bunifuFlatButton3.OnHoverTextColor = Color.White;
+            bunifuFlatButton3.Padding = new Padding(20, 0, 0, 0);
+            bunifuFlatButton3.selected = false;
+            bunifuFlatButton3.Size = new Size(210, 45);
+            bunifuFlatButton3.TabIndex = 5;
+            bunifuFlatButton3.Text = "       Home";
+            bunifuFlatButton3.TextAlign = ContentAlignment.MiddleLeft;
+            bunifuFlatButton3.Textcolor = Color.White;
+            bunifuFlatButton3.TextFont = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            // 
+            // bunifuFlatButton4
+            // 
+            bunifuFlatButton4.Active = false;
+            bunifuFlatButton4.Activecolor = Color.FromArgb(46, 139, 87);
+            bunifuFlatButton4.BackColor = Color.FromArgb(163, 192, 245);
+            bunifuFlatButton4.BackgroundImageLayout = ImageLayout.Stretch;
+            bunifuFlatButton4.BorderRadius = 0;
+            bunifuFlatButton4.ButtonText = "       Home";
+            bunifuFlatButton4.DisabledColor = Color.Gray;
+            bunifuFlatButton4.Iconcolor = Color.FromArgb(163, 192, 245);
+            bunifuFlatButton4.Iconimage = Properties.Resources.setting_icon;
+            bunifuFlatButton4.Iconimage_right = null;
+            bunifuFlatButton4.Iconimage_right_Selected = null;
+            bunifuFlatButton4.Iconimage_Selected = null;
+            bunifuFlatButton4.IconMarginLeft = 0;
+            bunifuFlatButton4.IconMarginRight = 0;
+            bunifuFlatButton4.IconRightVisible = true;
+            bunifuFlatButton4.IconRightZoom = 0D;
+            bunifuFlatButton4.IconVisible = true;
+            bunifuFlatButton4.IconZoom = 55D;
+            bunifuFlatButton4.IsTab = false;
+            bunifuFlatButton4.Location = new Point(0, 285);
+            bunifuFlatButton4.Margin = new Padding(0);
+            bunifuFlatButton4.Name = "bunifuFlatButton4";
+            bunifuFlatButton4.Normalcolor = Color.FromArgb(163, 192, 245);
+            bunifuFlatButton4.OnHovercolor = Color.FromArgb(117, 160, 240);
+            bunifuFlatButton4.OnHoverTextColor = Color.White;
+            bunifuFlatButton4.Padding = new Padding(20, 0, 0, 0);
+            bunifuFlatButton4.selected = false;
+            bunifuFlatButton4.Size = new Size(210, 45);
+            bunifuFlatButton4.TabIndex = 6;
+            bunifuFlatButton4.Text = "       Home";
+            bunifuFlatButton4.TextAlign = ContentAlignment.MiddleLeft;
+            bunifuFlatButton4.Textcolor = Color.White;
+            bunifuFlatButton4.TextFont = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            // 
+            // MainContainerView
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1038, 561);
+            Controls.Add(flowLayoutPanel1);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "MainContainerView";
+            Text = "MainContainerView";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            flowLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private ReaLTaiizor.Controls.NightControlBox nightControlBox1;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Bunifu.Framework.UI.BunifuFlatButton bunifuFlatButton1;
+        private Bunifu.Framework.UI.BunifuFlatButton bunifuFlatButton2;
+        private FlowLayoutPanel flowLayoutPanel2;
+        private Bunifu.Framework.UI.BunifuFlatButton bunifuFlatButton3;
+        private Bunifu.Framework.UI.BunifuFlatButton bunifuFlatButton4;
+    }
+}
